cb862a05d85ad33bd71697f073105440465b05fc
Koray Liman <69529444+KorayLiman@users.noreply.github.com>
2022-01-22 11:31:04 +0300

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  MyApp({Key? key}) : super(key: key);
+  String _img =
+      "https://www.sivasizpsikoloji.com/gorseller/komponent/header--ortali-logo/komponent4412.png";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,16 +18,62 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.green,
         appBar: AppBar(
-          //backgroundColor: Colors.yellow,
-          title: const Text("Title")
+            //backgroundColor: Colors.yellow,
+            title: const Text("Title")),
+        body: Container(
+          color: Colors.blue.shade400,
+          child: Column(
+            // mainAxisSize: MainAxisSize.max,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("K"),
+                  Text("o"),
+                  Text("r"),
+                  Text("a"),
+                  Text("y"),
+                ],
+              ),
+              Icon(
+                Icons.circle,
+                size: 64,
+                color: Colors.red,
+              ),
+              Icon(
+                Icons.circle,
+                size: 64,
+                color: Colors.green,
+              )
+            ],
+          ),
         ),
-        body: Container( alignment: Alignment.center,child: const Text("Hello",),color: Colors.amber,),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            debugPrint("Clicked");
-          },
-          child: const Icon(Icons.home_filled)
-        ),
+            onPressed: () {
+              debugPrint("Clicked");
+            },
+            child: const Icon(Icons.home_filled)),
+      ),
+    );
+  }
+
+  Widget ContainerLessons() {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(left: 40, top: 100, right: 40, bottom: 100),
+        decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.red,
+            border: Border.all(width: 8, color: Colors.purple),
+            borderRadius: BorderRadius.circular(40),
+            image:
+                DecorationImage(image: NetworkImage(_img), fit: BoxFit.contain),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.purple, offset: Offset(20, 20), blurRadius: 15)
+            ]),
       ),
     );
   }
